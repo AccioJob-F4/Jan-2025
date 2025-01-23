d141a9c5858a161b31cd95aee720c49f8bd6c4fe
RgnDunes <divyansh.singh@razorpay.com>
2025-01-23 21:22:15 +0530

    add remaingin fn

--- Session 09/script.js
+++ Session 09/script.js
@@ -39,24 +39,6 @@ document.addEventListener("DOMContentLoaded", () => {
   applyTranslations(language);
 });
 
-taskForm.addEventListener("submit", addTask);
-taskList.addEventListener("click", modifyTask);
-taskList.addEventListener("change", toggleTaskCompletion);
-searchBar.addEventListener("input", searchTasks);
-filterCategory.addEventListener("change", filterTasks);
-filterStatus.addEventListener("change", filterTasks);
-sortPriority.addEventListener("change", sortTasksByPriority);
-themeToggle.addEventListener("change", toggleTheme);
-languageSelect.addEventListener("change", () => {
-  const selectedLanguage = languageSelect.value;
-  localStorage.setItem("language", selectedLanguage);
-  applyTranslations(selectedLanguage);
-});
-dashboardBtn.addEventListener("click", openDashboard);
-closeModal.addEventListener("click", () => {
-  dashboardModal.style.display = "none";
-});
-
 const addTask = (e) => {
   e.preventDefault();
   const task = {
@@ -68,6 +50,8 @@ const addTask = (e) => {
     completed: false,
     subtasks: [],
   };
+
+  console.log(task);
 
   tasks.push(task);
   saveTasks();
@@ -143,3 +127,274 @@ const displayTasks = (
 
   renderPaginationControls(taskArray.length, currentPage, pageSize);
 };
+
+const renderSubtask = (subtask) => {
+  return `
+  <li class="task-item subtask" data-id="${subtask.id}">
+  <input type="checkbox" class="complete-task" ${
+    subtask.completed ? "checked" : ""
+  }>
+  <h3 ${subtask.completed ? 'class="completed"' : ""}>${subtask.name}</h3>
+  <p>Category: ${subtask.category}</p>
+  <p>Deadline: ${subtask.deadline}</p>
+  <p>Priority: ${subtask.priority}</p>
+  </li>
+  `;
+};
+
+const renderTask = (task) => {
+  const li = document.createElement("li");
+  li.classList.add("task-item", task.priority.toLowerCase() + "-priority");
+  li.setAttribute("data-id", task.id);
+
+  li.innerHTML = `<input type="checkbox" class="complete-task" ${
+    task.completed ? "checked" : ""
+  }>
+  <h3 ${task.completed ? 'class="completed"' : ""}>${task.name}</h3>
+  <p>Category: ${task.category}</p>
+  <p>Deadline: ${task.deadline}</p>
+  <p>Priority: ${task.priority}</p>
+  <div class="task-actions">
+    <button class="add-subtask-btn" data-id="${task.id}">Add Subtask</button>
+    <button class="edit-btn">Edit</button>
+    <button class="delete-btn">Delete</button>
+  </div>
+  `;
+
+  if (task.subtasks && task.subtasks.length > 0) {
+    const accordian = document.createElement("div");
+    accordian.classList.add("subtask-accordian");
+    accordian.innerHTML = `
+      <button class="accordian-toggle">View Subtasks</button>
+      <div class="accordian-content">
+        <ul>${task.subtasks
+          .map((subtask) => {
+            return renderSubtask(subtask);
+          })
+          .join("")}</ul>
+      </div>
+    `;
+
+    li.appendChild(accordian);
+  }
+
+  return li;
+};
+
+const modifyTask = (e) => {
+  const taskItem = e.target.closest(".task-item");
+  const taskId = taskItem.dataset.id;
+
+  if (e.target.classList.contains("delete-btn")) {
+    deleteTask(taskId);
+  } else if (e.target.classList.contains("edit-btn")) {
+    editTask(taskId);
+  } else if (e.target.classList.contains("add-subtask-btn")) {
+    addSubtask(taskId);
+  }
+};
+
+const deleteTask = (id) => {
+  tasks = removeTaskById(tasks, id);
+  saveTask();
+  displayTasks(tasks);
+};
+
+const removeTaskById = (taskArray, id) => {
+  return taskArray.filter((task) => {
+    if (task.id === id) {
+      return false;
+    } else if (task.subtasks && task.subtasks.length > 0) {
+      task.subtasks = removeTaskById(task.subtasks, id);
+    }
+    return true;
+  });
+};
+
+const editTask = (id) => {
+  const task = findTaskById(tasks, id);
+
+  if (task) {
+    taskNameInput.value = task.name;
+    taskCategoryInput.value = task.category;
+    taskDeadlineInput.value = task.deadline;
+    taskPriorityInput.value = task.priority;
+    deleteTask(id);
+  }
+};
+
+const findTaskById = (taskArray, id) => {
+  for (let task of taskArray) {
+    if (task.id === id) {
+      return task;
+    } else if (task.subtasks && task.subtasks.length > 0) {
+      const found = findTaskById(task.subtasks, id);
+      if (found) return found;
+    }
+  }
+  return null;
+};
+
+const updateDisplayTasks = () => {
+  let updatedTasks = tasks.slice();
+
+  const keyword = searchBar.value.toLowerCase();
+  if (keyword) {
+    updatedTasks = updatedTasks.filter((task) =>
+      task.name.toLowerCase().includes(keyword)
+    );
+  }
+
+  const category = filterCategory.value;
+  if (category !== "All") {
+    updatedTasks = updatedTasks.filter((task) => task.category === category);
+  }
+
+  const status = filterStatus.value;
+  if (status === "Completed") {
+    updatedTasks = updatedTasks.filter((task) => task.completed);
+  } else if (status === "Incomplete") {
+    updatedTasks = updatedTasks.filter((task) => !task.completed);
+  }
+
+  const sortOrder = sortPriority.value;
+  if (sortOrder === "High") {
+    updatedTasks = updatedTasks.sort(
+      (a, b) => getPriorityLevel(a.priority) - getPriorityLevel(b.priority)
+    );
+  } else if (sortOrder === "Low") {
+    updatedTasks = updatedTasks.sort(
+      (a, b) => getPriorityLevel(b.priority) - getPriorityLevel(a.priority)
+    );
+  }
+
+  currentPage = 1;
+
+  displayTasks(updatedTasks, currentPage);
+};
+
+const addSubtask = (parentTaskId) => {
+  const subTaskName = prompt("Enter subtask name:");
+  if (subTaskName) {
+    const parentTask = findTaskById(tasks, parentTaskId);
+
+    if (parentTask) {
+      const subtask = {
+        id: Date.now(),
+        name: subTaskName,
+        category: parentTask.category,
+        deadline: parentTask.deadline,
+        priority: parentTask.priority,
+        completed: false,
+      };
+
+      parentTask.subtasks.push(subtask);
+
+      saveTasks();
+
+      updateDisplayTasks();
+    }
+  }
+};
+
+const searchTasks = () => {
+  const keyword = searchBar.value.toLowerCase();
+  const filteredTasks = tasks.filter((task) =>
+    task.name.toLowerCase().includes(keyword)
+  );
+
+  displayTasks(filteredTasks);
+};
+
+const filterTasks = () => {
+  const category = filterCategory.value;
+  const status = filterStatus.value;
+  let filteredTasks = tasks;
+
+  if (category !== "All") {
+    filteredTasks = filteredTasks.filter((task) => task.category === category);
+  }
+  if (status === "Completed") {
+    filteredTasks = filteredTasks.filter((task) => task.completed);
+  } else if (status === "Incomplete") {
+    filteredTasks = filteredTasks.filter((task) => !task.completed);
+  }
+
+  displayTasks(filteredTasks);
+};
+
+const saveTasks = () => {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+const toggleTaskCompletion = (e) => {
+  if (e.target.classList.contains("complete-task")) {
+    const taskItem = e.target.closest(".task-item");
+    const taskId = taskItem.dataset.id;
+
+    const task = findTaskById(tasks, taskId);
+
+    if (task) {
+      task.completed = e.target.checked;
+      saveTasks();
+      updateDisplayTasks();
+    }
+  }
+};
+
+const getPriorityLevel = (priority) => {
+  switch (priority) {
+    case "High":
+      return 1;
+    case "Medium":
+      return 2;
+    case "Low":
+      return 3;
+    default:
+      return 4;
+  }
+};
+
+const sortTasksByPriority = () => {
+  const sortOrder = sortPriority.value;
+  const sortedTasks = [...tasks];
+  if (sortOrder === "High") {
+    sortedTasks = sortedTasks.sort(
+      (a, b) => getPriorityLevel(a.priority) - getPriorityLevel(b.priority)
+    );
+  } else if (sortOrder === "Low") {
+    sortedTasks = sortedTasks.sort(
+      (a, b) => getPriorityLevel(b.priority) - getPriorityLevel(a.priority)
+    );
+  }
+
+  displayTasks(sortedTasks);
+};
+
+const toggleTheme = () => {
+  if (themeToggle.checked) {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
+  }
+};
+
+taskForm.addEventListener("submit", addTask);
+taskList.addEventListener("click", modifyTask);
+taskList.addEventListener("change", toggleTaskCompletion);
+searchBar.addEventListener("input", searchTasks);
+filterCategory.addEventListener("change", filterTasks);
+filterStatus.addEventListener("change", filterTasks);
+sortPriority.addEventListener("change", sortTasksByPriority);
+themeToggle.addEventListener("change", toggleTheme);
+languageSelect.addEventListener("change", () => {
+  const selectedLanguage = languageSelect.value;
+  localStorage.setItem("language", selectedLanguage);
+  applyTranslations(selectedLanguage);
+});
+dashboardBtn.addEventListener("click", openDashboard);
+closeModal.addEventListener("click", () => {
+  dashboardModal.style.display = "none";
+});
